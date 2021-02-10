1fdbf5c42d84268207e578259e6974a8347626d1
Zharckov <23assasyncred03@gmail.com>
2021-02-10 06:46:59 +0300

    get_servers_info

--- io/index.js
+++ io/index.js
@@ -1,10 +1,18 @@
 const { Server }    = require('socket.io');
+const servers       = require('../utils/servers');
 
 const io            = new Server();
 
 io.on('connection', (socket) => {
 
-    socket.on('get_servers_info', () => {});
+    socket.on('get_servers_info', async (callback) => {
+        try {
+            let serverInfo = await servers.getServers();
+            // Send info about servers 
+        } catch (error) {
+
+        }
+    });
 
     socket.on('disconnect', (reason) => {
         console.log(`Socket-Client disconnect: ${reason}`);
