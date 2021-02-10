4522d2a342d131aff6293c18c6e92d9d7255915e
Zharckov <23assasyncred03@gmail.com>
2021-02-10 06:43:11 +0300

    isAuthed fix

--- middlewares/ifAuthed.js
+++ middlewares/ifAuthed.js
@@ -7,6 +7,11 @@ module.exports = (req, res, next) => {
         req.user = info;
         return next();
     } catch(error){
+        if(error.name) { 
+            if(error.name == 'TokenExpiredError')
+                return res.status(401).json({ message: "Token expired"});
+            return res.status(500).json({ message: "JWT Error"});
+        }
         return res.status(401).json({ message: "Invalid token"});
     }
 }
