d746b6fdd86f684cd81d68906fee51ae9d35e125
Zharckov <23assasyncred03@gmail.com>
2021-02-10 03:00:41 +0300

    add db-connect

--- database/database.js
+++ database/database.js
@@ -3,4 +3,13 @@ const { Sequelize, Model, DataTypes }   = require('sequelize');
 
 const sequelize = new Sequelize(config.database);
 
+(async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+    } catch (error) {
+        throw new Error(`Не удалось подлючиться к БД: ${error}`);
+    }
+})();
+
 module.exports = { Model, DataTypes, sequelize }
