d4ab8594d116b9d6e50772212583a2f9750dc41d
Zharckov <23assasyncred03@gmail.com>
2021-02-28 15:45:56 +0300

    add streams route. rename streamers route to stream

--- routes/manage.js
+++ routes/manage.js
@@ -4,8 +4,8 @@ _route.get('/news', async (req, res) => {});
 _route.post('/news', async (req, res) => {});
 _route.delete('/news/:id', async (req, res) => {});
 
-_route.get('/streamers', async (req, res) => {});
-_route.post('/streamers', async (req, res) => {});
-_route.delete('/streamers/:id', async (req, res) => {});
+_route.get('/streams', async (req, res) => {});
+_route.post('/streams', async (req, res) => {});
+_route.delete('/streams/:id', async (req, res) => {});
 
 module.exports = _route;
---
+++ routes/streams.js
@@ -0,0 +1,5 @@
+const _route = require('express').Router();
+
+_route.get('/streams', async (req, res) => {});
+
+module.exports = _route;
