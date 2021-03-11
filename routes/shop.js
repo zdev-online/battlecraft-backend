94df61f307844da94fac5ba5ac6a0e4b9f58e480
Zharckov <23assasyncred03@gmail.com>
2021-03-11 11:30:19 +0300

    Add comments to News

--- routes/shop.js
+++ routes/shop.js
@@ -8,9 +8,17 @@ const Donate    = require('../database/models/Donate');
 const ifAuthed  = require('../middlewares/ifAuthed');
 const servers   = require('../servers.json');
 
+// Получить страницу товаров 
 _route.get('/', async (req, res) => { 
     try {
-        let data = await paginate(Products, req.query.page || 1, {}); 
+        // req.query.server - id сервера из servers.json, а именно model_name
+        // Получить список доступных серверов: GET -> localhost:4000/servers
+        let server = req.query.server ? { server: req.query.server} : {}; 
+        // data:
+        //  current_page - текущая страница
+        //  page_count - кол-во страниц
+        //  data - данные товара
+        let data = await paginate(Products, req.query.page || 1, server); 
         return res.json(data);
     } catch (error) { return errorHear.hear(res, error) }
 });
