404d87b78dc00a8b130e5d59e77b5c984ad0ce11
Zharckov <23assasyncred03@gmail.com>
2021-03-13 20:04:43 +0300

    fix russians symbols in database

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const { Sequelize, Model, DataTypes }   = require('sequelize');
 
 const sequelize = new Sequelize(config.database);
 
-sequelize.authenticate({ logging: false }).catch(error => {
+sequelize.authenticate({}).catch(error => {
     console.error(`Не удалось подлючиться к БД: ${error.message}`);
     console.error(`Stack: ${error.stack}`);
 }).then(() => {
