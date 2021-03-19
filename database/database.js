e356bf8f1712778dfed19ed4e5a592efdf4af68e
Arillaxe <bekirshabut66@gmail.com>
2021-03-19 04:51:31 +0300

    Added docker

--- database/database.js
+++ database/database.js
@@ -1,7 +1,27 @@
 const config    = require('../config.json');    
 const { Sequelize, Model, DataTypes }   = require('sequelize');
 
-const sequelize = new Sequelize(config.database);
+const {
+    host,
+    port,
+    username,
+    password,
+    database,
+    dialect,
+    logging,
+    ...rest
+} = config.database;
+
+const sequelize = new Sequelize({
+    host    : process.env.DB_HOST     || host,
+    port    : process.env.DB_PORT     || port,
+    database: process.env.DB_DATABASE || database,
+    username: process.env.DB_USERNAME || username,
+    password: process.env.DB_PASSWORD || password,
+    dialect : process.env.DB_DIALECT  || dialect,
+    logging : process.env.DB_LOGGING  || logging,
+    ...rest,
+});
 
 sequelize.authenticate({ logging: false }).catch(error => {
     console.error(`Не удалось подлючиться к БД: ${error.message}`);
