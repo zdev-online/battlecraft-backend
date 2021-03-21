1d7e4a407d01641fe72a1dee0735501ec7c7d146
Zharckov <23assasyncred03@gmail.com>
2021-03-21 14:56:30 +0300

    Start Write Payeer-System

--- modules/Payeer.js
+++ modules/Payeer.js
@@ -3,13 +3,19 @@ const axios     = require('axios').default;
 const config    = require('../config.json');
 
 class Payeer {
-    async getPaymentLink(){}
-    handlers(){
-        return {
-            async success(req, res){},
-            async fail(req, res){},
-            async proccess(req, res){}
+    async getPaymentLink(email, price){
+        let data = {
+            ...config.payeer,
+            action: "invoiceCreate",
+            m_orderid: email,
+            m_amount: price,
+            m_curr: 'RUB' 
         }
+        let link = await axios.post("https://payeer.com/ajax/api/api.php?invoiceCreate", data); 
+        
+    }
+    async handler(req, res){
+        
     }
 }
 
